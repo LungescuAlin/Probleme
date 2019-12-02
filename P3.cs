a63c9bf7034f20bfdaff982a6d9eb9764b2e8c20
Lungescu Alin <lungescualin1997@gmail.com>
2019-12-02 13:37:00 +0200

    Problema 3

---
+++ P3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace teme_ffs
+{
+    public class P3
+    {
+        public List<int> b = new List<int>();
+        public bool IsSymmetrical(int n)
+        {
+            while(n>0)
+            {
+                b.Add(n % 10);
+                n /= 10;
+            };
+
+            for(int i=0;i<b.Count/2;i++)
+            {
+                if (b[i] != b[b.Count - 1 - i])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
