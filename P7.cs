291b86bce8261238db52d72fbf2a804ded4c9cd2
Lungescu Alin <lungescualin1997@gmail.com>
2019-12-02 13:37:29 +0200

    Problema 7

---
+++ P7.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace teme_ffs
+{
+    public class P7
+    {
+       
+        public int MysteryFunction( int n)
+        {
+            int mf=1;
+            while(n>0)
+            {
+                mf *= n % 10;
+                n /=10;
+            }
+            return mf;
+        }
+    }
+}
