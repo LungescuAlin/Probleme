ca239695a0e0f7ca1f3ab69383440f088cc2c80a
Lungescu Alin <lungescualin1997@gmail.com>
2019-12-02 13:36:48 +0200

    Problema 2

---
+++ P2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace teme_ffs
+{
+    public class P2
+    {
+        public string Convb(int n)
+        {
+            string b="";
+            string r = "";
+            while(n>0)
+            {
+                b += n % 2;
+                
+                n /= 2;
+            }
+            for (int i = b.Length - 1; i >= 0; i--)
+                r += b[i];
+            return r;
+            
+        }
+       
+    }
+}
