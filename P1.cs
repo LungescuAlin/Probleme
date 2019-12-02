09af06c05489e3b61663f1e3883caeb778b6b6fa
Lungescu Alin <lungescualin1997@gmail.com>
2019-12-02 13:28:20 +0200

    Problema 1

---
+++ P1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace teme_ffs
+{
+    public class P1
+    {
+        
+        public int[,] Create(int n)
+        {
+            int[,] array = new int[100,100];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n - i; j++)
+                    array[i, j] = n;
+            return array;
+        }
+    }
+}
