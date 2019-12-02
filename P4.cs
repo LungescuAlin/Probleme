dcb8bd1fbbe62885e12cf12d468adaaf8d635985
Lungescu Alin <lungescualin1997@gmail.com>
2019-12-02 13:37:08 +0200

    Problema 4

---
+++ P4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace teme_ffs
+{
+    public class P4
+    {
+       
+        public bool ValidateEmail(string s)
+        {
+            int a = s.IndexOf("@");
+            int b = s.IndexOf(".");
+            if (a<=0||b<=0||b<a)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
