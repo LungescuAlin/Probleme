7bcf6941930d9480ec6f2793ec38f19969997222
Lungescu Alin <lungescualin1997@gmail.com>
2019-12-02 13:37:16 +0200

    Problema 5

---
+++ P5.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace teme_ffs
+{
+    public class P5
+    {
+        public string MakeTitle(string txt)
+        {
+            string r = "";
+            if (txt.Length != 0)
+            {  r = "" + char.ToUpper(txt[0]);
+                if (txt.Length!=1)
+                for (int i = 1; i < txt.Length; i++)
+                {
+                    r += txt[i];
+
+                        if (txt[i] == 32)
+                        {
+                            if (txt[txt.Length-1] != txt[i])
+                            {
+                                r += char.ToUpper(txt[i + 1]);
+                                i++;
+                            }
+                            
+                        }
+                
+                    }
+            }
+
+            return r;
+        }
+    }
+}
