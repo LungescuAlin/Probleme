3418dca539713ae4cf26bfdc504a8af4caccdab4
Lungescu Alin <lungescualin1997@gmail.com>
2019-12-02 13:37:22 +0200

    Problema 6

---
+++ P6.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace teme_ffs
+{
+    public class P6
+    {
+        public string Accum(string txt)
+        {
+            string r = "";
+            for(int i=0;i<txt.Length;i++)
+            {
+                for(int j=0;j<=i;j++)
+                {
+                    if(j==0)
+                    {
+                        r += char.ToUpper(txt[i]);
+                    }
+                    else
+                    {
+                        r += char.ToLower(txt[i]);
+                    }
+                }
+                if(i<txt.Length-1)
+                
+                    r += '-';
+            }
+            return r;
+        }
+    }
+}
